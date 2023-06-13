6e68ccf33ebf1e192679ae924b2e261cfcd1eda5
Sushant GUJAR <35103473+sushantgujar04@users.noreply.github.com>
2023-06-13 22:54:59 +0530

    Update JWTSettings.cs
    Added "Isvalid" setting key for jwt token

--- Domain/Settings/JWTSettings.cs
+++ Domain/Settings/JWTSettings.cs
@@ -10,5 +10,6 @@ namespace Domain.Settings
         public string Issuer { get; set; }
         public string Audience { get; set; }
         public double DurationInMinutes { get; set; }
+        public bool Isvalid { get; set; }
     }
 }
